d01e39a2ee0f00d1b3c3f78aa3a25c01f1e0559f
Mike Purvis <mikedotexe@gmail.com>
2022-07-24 16:33:19 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/csli-logo-transparent-no-border.png';
+import coloUrs from './assets/csli-colors.png'
+import burnImageKindaCool from './assets/csli-logo-burn.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="featured-image">
+        <img src={burnImageKindaCool} />
+      </div>
+      <div id="colors">
+        These'll do, 🐷:
+        <img src={coloUrs}/><br/>
+        <p>Feel free to play with these at:</p>
+        <a href="https://color.adobe.com/create/color-wheel">https://color.adobe.com/create/color-wheel</a>
+      </div>
+
     </div>
   );
 }
